0720b8991627460c6ca4e092cce14f3f6d7560f3
최서정 <cchloe2311@daum.net>
2021-03-10 11:22:01 +0900

    added count -> if rotate, it reset n

--- app/src/main/java/com/example/orientationpractice/MainActivity.kt
+++ app/src/main/java/com/example/orientationpractice/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.orientationpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var n = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+    fun count(v: View) {
+        tv_count.text = (++n).toString()
+    }
+
 }
